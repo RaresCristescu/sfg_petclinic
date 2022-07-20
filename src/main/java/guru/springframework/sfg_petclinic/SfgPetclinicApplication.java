c61c05b22f607699cc7cca9723e55b65b9e34f9f
RaresCristescu <rares.cristescu123@gmail.com>
2022-07-20 09:57:51 +0300

    Added console log

--- src/main/java/guru/springframework/sfg_petclinic/SfgPetclinicApplication.java
+++ src/main/java/guru/springframework/sfg_petclinic/SfgPetclinicApplication.java
@@ -8,6 +8,8 @@ public class SfgPetclinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SfgPetclinicApplication.class, args);
+		
+		System.out.println("uhu");
 	}
 
 }
